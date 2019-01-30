4587f1f13f817d844b957a90f1c10ce8087f86d4
Mickey <micksheridan.24@gmail.com>
2019-01-30 12:16:09 -0500

    submit

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  ret = {}
+  data[:gender].each do |gender, pidgeons|
+    pidgeons.each do |p|
+      ret[p] = {}
+      ret[p][:gender] = [gender.to_s]
+      ret[p][:color] = []
+      ret[p][:lives] = []
+    end
+  end
+  
+  data[:color].each do |color, pidgeons|
+    pidgeons.each do |p|
+
+      ret[p][:color] << color.to_s
+    end
+  end
+  data[:lives].each do |lives, pidgeons|
+    pidgeons.each do |p|
+
+      ret[p][:lives] << lives.to_s
+    end
+  end
+
+  ret
 end
